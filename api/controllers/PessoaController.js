6ca01ed3ae63664914eb7a249b5f84fa386ce189
fabrizioabreu <fabrizioabreu@live.com>
2023-05-10 21:16:46 -0300

    Insert Pessoa

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -22,8 +22,20 @@ class PessoaController {
       const umaPessoa = await database.Pessoas.findOne({
         where: { id: Number(id) }
       })
-
       return res.status(200).json(umaPessoa)
+
+    } catch (error) {
+      return res.status(500).json(error.message)
+    }
+  }
+
+  static async criarPessoa(req, res) {
+
+    const novaPessoa = req.body
+
+    try {
+      const novaPessoaCriada = await database.Pessoas.create(novaPessoa)
+      return res.status(200).json(novaPessoaCriada)
 
     } catch (error) {
       return res.status(500).json(error.message)
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -5,5 +5,6 @@ const router = Router()
 
 router.get('/pessoas', PessoaController.pegaTodasAsPessoas)
 router.get('/pessoas/:id', PessoaController.pegaUmaPessoa)
+router.post('/pessoas', PessoaController.criarPessoa)
 
 module.exports = router
