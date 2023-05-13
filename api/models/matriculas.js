93a047f4a41057ce16982c54ca3f7504cf146b79
fabrizioabreu <fabrizioabreu@live.com>
2023-05-12 21:28:00 -0300

    Referenciando tabelas

--- api/models/matriculas.js
+++ api/models/matriculas.js
@@ -3,8 +3,9 @@ module.exports = (sequelize, DataTypes) => {
   const Matriculas = sequelize.define('Matriculas', {
     status: DataTypes.STRING
   }, {});
-  Matriculas.associate = function(models) {
-    // associations can be defined here
+  Matriculas.associate = function (models) {
+    Matriculas.belongsTo(models.Pessoas, { foreignKey: 'estudante_id' })
+    Matriculas.belongsTo(models.Turmas, { foreignKey: 'turma_id' })
   };
   return Matriculas;
 };
--- api/models/niveis.js
+++ api/models/niveis.js
@@ -3,8 +3,8 @@ module.exports = (sequelize, DataTypes) => {
   const Niveis = sequelize.define('Niveis', {
     descr_nivel: DataTypes.STRING
   }, {});
-  Niveis.associate = function(models) {
-    // associations can be defined here
+  Niveis.associate = function (models) {
+    Niveis.hasMany(models.Turmas, { foreignKey: 'nivel_id' })
   };
   return Niveis;
 };
